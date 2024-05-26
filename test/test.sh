6390dc830366d468b97675aeb1b95bf1fb9c728a
Bùi Tiến Sâm 22022517 <120190847+sambt22022517@users.noreply.github.com>
2024-05-26 16:55:00 +0700

    Update test.sh

--- test/test.sh
+++ test/test.sh
@@ -8,8 +8,8 @@ if $(hdfs dfs -test -d $hdfs_output_folder/out_posterior) ; then hdfs dfs -rm -r
 mapred streaming \
 	-file $test_home/mapper_test.py \
 	-file $test_home/reducer_test.py \
-	-mapper mapper_test.py \
-	-reducer reducer_test.py \
+	-mapper $test_home/mapper_test.py \
+	-reducer $test_home/reducer_test.py \
 	-cacheFile $hdfs_output_folder/out_conditional/part-00000#conditional \
 	-cacheFile $hdfs_output_folder/out_prior2/part-00000#prior \
 	-input $hdfs_input_file \
