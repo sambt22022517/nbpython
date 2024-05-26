11910a095d076ad5bd00e034058e288ace35218f
Bùi Tiến Sâm 22022517 <120190847+sambt22022517@users.noreply.github.com>
2024-05-26 16:55:30 +0700

    Update train.sh

--- train/train.sh
+++ train/train.sh
@@ -12,40 +12,40 @@ if $(hdfs dfs -test -d $output_folder/out_conditional); then hdfs dfs -rm -r $ou
 mapred streaming \
 	-file $train_home/mapper.py \
 	-file $train_home/reducer.py \
-	-mapper mapper.py \
-	-reducer reducer.py \
+	-mapper $train_home/mapper.py \
+	-reducer $train_home/reducer.py \
 	-input $input_file \
 	-output $output_folder/out1
 
 mapred streaming \
 	-file $train_home/mapper2.py \
 	-file $train_home/reducer2.py \
-	-mapper mapper2.py \
-	-reducer reducer2.py \
+	-mapper $train_home/mapper2.py \
+	-reducer $train_home/reducer2.py \
 	-input $output_folder/out1/part-00000 \
 	-output $output_folder/out2
 
 mapred streaming \
 	-file $train_home/mapper_prior.py \
 	-file $train_home/reducer_prior.py \
-	-mapper mapper_prior.py \
-	-reducer reducer_prior.py \
+	-mapper $train_home/mapper_prior.py \
+	-reducer $train_home/reducer_prior.py \
 	-input $input_file \
 	-output $output_folder/out_prior
 
 mapred streaming \
 	-file $train_home/mapper_prior2.py \
 	-file $train_home/reducer_prior2.py \
-	-mapper mapper_prior2.py \
-	-reducer reducer_prior2.py \
+	-mapper $train_home/mapper_prior2.py \
+	-reducer $train_home/reducer_prior2.py \
 	-input $output_folder/out_prior/part-00000 \
 	-output $output_folder/out_prior2
 
 mapred streaming \
 	-file $train_home/mapper_conditional.py \
 	-file $train_home/reducer_conditional.py \
-	-mapper mapper_conditional.py \
-	-reducer reducer_conditional.py \
+	-mapper $train_home/mapper_conditional.py \
+	-reducer $train_home/reducer_conditional.py \
 	-input $output_folder/out1/part-00000 \
 	-input $output_folder/out2/part-00000 \
 	-output $output_folder/out_conditional
